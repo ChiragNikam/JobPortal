b14597fd37886f897ed919c87f8323dae40247e9
Chirag Nikam <chiragnikam02@gmail.com>
2024-01-19 16:34:57 +0530

    feature - recruiter can send email to the applier if the applier get's selected for the interview process.

--- app/src/main/java/com/brillect/jobportal/UIComponents/RecruiterUI/AppliedCandidateInfoViews.kt
+++ app/src/main/java/com/brillect/jobportal/UIComponents/RecruiterUI/AppliedCandidateInfoViews.kt
@@ -1,5 +1,7 @@
 package com.brillect.jobportal.UIComponents.RecruiterUI
 
+import android.content.Intent
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
@@ -18,9 +20,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.brillect.jobportal.Applier.ApplierProfileViewModel
 import com.brillect.jobportal.Recruiter.AppliedCandidateViewModel
+import com.brillect.jobportal.UIComponents.BtnCustom
 import com.brillect.jobportal.UIComponents.InfoBlock
 import com.brillect.jobportal.ui.theme.BackgroundColor
 import com.rizzi.bouquet.HorizontalPDFReader
@@ -39,6 +43,8 @@ fun CandidateProfile(viewModel: AppliedCandidateViewModel) {
             .padding(start = 24.dp, top = 24.dp, end = 24.dp)
     ) {
         val userProfile by viewModel.candidateInfo.collectAsState()
+
+        val context = LocalContext.current
 
         // User Name
         InfoBlock(
@@ -74,12 +80,34 @@ fun CandidateProfile(viewModel: AppliedCandidateViewModel) {
             label = "Skills",
             description = userProfile.skills
         )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        BtnCustom(onClicking = {
+            // on below line we are creating
+            // an intent to send an email
+            val i = Intent(Intent.ACTION_SEND)
+
+            // on below line we are passing email address,
+            // email subject and email body
+            i.putExtra(Intent.EXTRA_EMAIL, arrayOf(userProfile.email))
+            i.putExtra(Intent.EXTRA_SUBJECT, "Short Listed for Interview")
+            i.putExtra(Intent.EXTRA_TEXT, "Body")
+
+            // on below line we are
+            // setting type of intent
+            i.setType("message/rfc822")
+
+            // on the below line we are starting our activity to open email application.
+            context.startActivity(Intent.createChooser(i, "Choose an Email client : "))
+        }, text = "Send Email", padStart = 100, padEnd = 100)
+
         Spacer(modifier = Modifier.height(24.dp))
     }
 }
 
 @Composable
-fun CandidateResume(viewModel: AppliedCandidateViewModel, applierId: String){
+fun CandidateResume(viewModel: AppliedCandidateViewModel, applierId: String) {
     val resumeUrl by viewModel.resumeUrl.collectAsState()
 
     // pdf reader state
